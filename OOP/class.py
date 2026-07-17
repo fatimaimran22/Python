4e3515147f9373428730f29ccbf2fab4a7cdd970
Fatima Imran <fatimaimranofficials@gmail.com>
2026-07-17 18:32:43 +0500

    Class

---
+++ OOP/class.py
@@ -0,0 +1,22 @@
+class CLassName:
+    def __new__(cls):
+        obj=super().__new__(cls)
+        print("new called")
+        return obj
+    
+    def __init__(self):
+        print("init called")
+
+    def __init__(self, args):
+        pass
+
+    @classmethod
+    def classMethod(cls):
+        pass
+
+    @staticmethod
+    def staticMethod():
+        pass
+
+obj=CLassName()
+
