b7f3279181e4845cf9ef293918a7166dc6c9a917
Fatima Imran <fatimaimranofficials@gmail.com>
2026-07-20 16:04:44 +0500

    Deep, Shallow, Assignment

--- shallow_and_deep_copy.py
+++ shallow_and_deep_copy.py
@@ -1,25 +1,24 @@
+print("------Assignment---------")
 numbers=[10,20,30]
 a=numbers
 a.append(40)
-
 print(numbers)
 print(a)
 print(id(numbers))
 print(id(a))
 
-
+print("------Shallow---------")
 import copy
 b=copy.copy(numbers)
-
 b.append(50)
 print(numbers)
 print(b)
 print(id(numbers))
 print(id(b))
 
+print("------Deep---------")
 num=[[1,2],[3,4]]
 b=copy.deepcopy(num)
-
 b[0].append(100)
 print(num)
 print(b)
