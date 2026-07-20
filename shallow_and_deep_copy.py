63c2184ec12abb5b2aa640427732af93a628b606
Fatima Imran <fatimaimranofficials@gmail.com>
2026-07-20 16:03:48 +0500

    Deep, Shallow, Assignment

---
+++ shallow_and_deep_copy.py
@@ -0,0 +1,27 @@
+numbers=[10,20,30]
+a=numbers
+a.append(40)
+
+print(numbers)
+print(a)
+print(id(numbers))
+print(id(a))
+
+
+import copy
+b=copy.copy(numbers)
+
+b.append(50)
+print(numbers)
+print(b)
+print(id(numbers))
+print(id(b))
+
+num=[[1,2],[3,4]]
+b=copy.deepcopy(num)
+
+b[0].append(100)
+print(num)
+print(b)
+print(id(num))
+print(id(b))
