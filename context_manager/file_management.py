5353b055853bc78c2c3a962e0eab02ebace0f49b
Fatima Imran <fatimaimranofficials@gmail.com>
2026-07-24 17:14:25 +0500

    Context Manager

---
+++ context_manager/file_management.py
@@ -0,0 +1,18 @@
+class FileManager():
+    def __init__(self,filename, mode):
+        self.filename=filename
+        self.mode=mode
+        self.file=None
+
+    def __enter__(self):
+        self.file=open(self.filename, self.mode)
+        return self.file
+    
+    def __exit__(self, exc_type, exc, tb):
+        self.file.close()
+
+
+with FileManager("file.txt","w") as file:
+    file.write("Hello")
+
+print(file.closed)
